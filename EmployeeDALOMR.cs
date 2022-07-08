41fe388fe5dfd82cfccb5d0d7e08ba50c90a2e5e
PranaliBhujbal <108925893+PranaliBhujbal@users.noreply.github.com>
2022-07-08 15:53:32 +0530

    Update EmployeeDALOMR.cs
    small changes in code

--- EmployeeDALOMR.cs
+++ EmployeeDALOMR.cs
@@ -6,13 +6,12 @@ using System.Web;
 using OMRWebApp.Models;
 using System.Data.Entity;
 using MySql.Data.EntityFramework;
-// this a emp class
+//changes done by Pranali
 namespace OMRWebApp.DAL
 {
     [DbConfigurationType(typeof(MySqlEFConfiguration))]
     public class EmployeeDALOMR: DbContext
     {
-        public DbSet<EmployeeBOL> emp { get; set; }
         public DbSet<EmployeeBOL> emp { get; set; }
         public EmployeeDALOMR() : base("WebDataCon")
         {
